a747f3d681a950cf39770bc0710d4e7eac4f37d0
Radoslaw Subczynski <kozak221@gmail.com>
2020-06-06 00:02:41 +0200

    test connect with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'mealList';
+  connected = "false"
+
+  configUrl = 'https://meal-list-backend.herokuapp.com/';
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(this.configUrl).subscribe(
+      () => {
+        this.connected = "true";
+      },
+      () => this.connected = "server is dead"
+    );
+  }
+
 }
