af287b4f59ad1155f2cba53c7ccc7b3bdc12dc42
Radoslaw Subczynski <kozak221@gmail.com>
2020-06-05 23:26:05 +0200

    test second config wersion

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/meal-list'));
+app.use(express.static('./dist/mealList'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/meal-list/'}),
+    res.sendFile('index.html', {root: 'dist/mealList/'}),
 );
 
 app.listen(process.env.PORT || 8080);
