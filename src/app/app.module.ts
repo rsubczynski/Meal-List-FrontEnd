b070564e084977d6c9b6218893e7749f5790323c
Radoslaw Subczynski <kozak221@gmail.com>
2020-06-07 13:19:31 +0200

    add primeNg liblary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import {AccordionModule} from 'primeng/accordion';   
+
 
 @NgModule({
   declarations: [
@@ -11,8 +15,10 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
